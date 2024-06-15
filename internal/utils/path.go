3e3c89a11161446949cb45c32822e9c6c565d635
arpan <arpan.rec@gmail.com>
2024-06-15 22:59:02 +0530

    feat: doc

--- internal/utils/path.go
+++ internal/utils/path.go
@@ -2,49 +2,31 @@ package utils
 
 import (
 	"errors"
-	"os"
-	"os/exec"
 	"path/filepath"
 	"strings"
 )
 
 func AbsPath(p string) (string, error) {
 
-	if strings.HasPrefix(p, "~/") || p == "~" ||
-		strings.HasSuffix(p, "/~") || strings.Contains(p, "/~/") {
-		homeDir, errHomeDir := os.UserHomeDir()
-		if errHomeDir != nil {
-			return "", errors.New("failed to get user home directory, " + errHomeDir.Error())
-		}
+	if strings.HasPrefix(p, "~/") || p == "~" {
 		if strings.HasPrefix(p, "~/") {
 			p = strings.Replace(p, "~/", homeDir+"/", 1)
 		}
 		if p == "~" {
 			p = homeDir
 		}
-		if strings.HasSuffix(p, "/~") {
-			p = strings.Replace(p, "/~", "/"+homeDir, 1)
-		}
-
-		if strings.Contains(p, "/~/") {
-			p = strings.ReplaceAll(p, "/~/", "/"+homeDir+"/")
-
-		}
 	}
 	if strings.Contains(p, "$") {
-		var envVars []string
-		copy(envVars, os.Environ())
-		cmd := exec.Command("/bin/bash", "-c", "realpath "+p)
-		cmd.Env = envVars
-		out, err := cmd.CombinedOutput()
+		cmd := "echo " + p
+		out, err := BashExec(&cmd)
 		if err != nil {
-			return "", errors.New("failed to get absolute path of SSH key using realpath, " + err.Error())
+			return "", errors.New("failed to get absolute path using shell echo: , " + err.Error())
 		}
-		p = strings.TrimSpace(string(out))
+		p = strings.TrimSpace(out)
 	}
 	absPath, errAbs := filepath.Abs(p)
 	if errAbs != nil {
-		return "", errors.New("failed to get absolute path of SSH key, " + errAbs.Error())
+		return "", errors.New("failed to get absolute path of the file, " + errAbs.Error())
 	}
 	p = absPath
 	return p, nil
